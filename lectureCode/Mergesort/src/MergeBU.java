610594f97e9707b6516c160b3cbcc43ae8d2811c
WEN1WE <1935862865@qq.com>
2019-05-08 21:07:02 +0800

    Merge

---
+++ lectureCode/Mergesort/src/MergeBU.java
@@ -0,0 +1,37 @@
+public class MergeBU {
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N -1));
+            }
+        }
+    }
+
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        assert isSorted(a, lo, mid);      // precondition: a[lo..mid] sorted
+        assert isSorted(a, mid + 1, hi);  // precondition: a[mid+1..hi] sorted
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+
+        assert isSorted(a, lo, hi);       // precondition: a[lo..hi] sorted
+    }
+}
