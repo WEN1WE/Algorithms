c27b797e101282ab45f704afb12a049b6e4d6371
WEN1WE <wwen8973@gmail.com>
2019-05-12 11:27:57 +0800

    init

---
+++ Programming Assignment/8 Puzzle/src/Board.java
@@ -0,0 +1,19 @@
+public class Board {
+    private final int[][] blocks;
+
+    public Board(int[][] blocks) {
+        this.blocks = blocks;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public int manhatten() {
+        return 0;
+    }
+
+    public boolean isGoal() {
+        return true;
+    }
+}
